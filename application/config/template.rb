fd39437728578342929186c180e7c01244f8db9b
Igor <igor.alexandrov@gmail.com>
2022-10-16 22:12:42 +0400

    Use RSpec by default in scaffold generators

--- application/config/template.rb
+++ application/config/template.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 template 'config/initializers/sidekiq.rb.tt' if requires_sidekiq?
+template 'config/initializers/generators.rb.tt'
 
 apply 'config/application.rb'
 apply 'config/environments/development.rb'
+apply 'config/environments/production.rb'
