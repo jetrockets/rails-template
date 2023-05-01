65270f503a5591e098696cf7d0122a4ea5722b7c
Dmitry Voronov <dmdropkick@gmail.com>
2023-05-01 18:47:38 +0400

    Add GitHub settings: Pull Request template

---
+++ application/github/template.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+def apply_template!
+  ask_for_github_pull_request_template
+
+  copy_github_pull_request_template if requires_github_pull_request_template?
+end
+
+def ask_for_github_pull_request_template
+  @requires_github_pull_request_template ||= ask_with_default('Do you want to add PR template?', :green, 'yes')
+end
+
+def requires_github_pull_request_template?
+  @requires_github_pull_request_template.strip.downcase == 'yes'
+end
+
+def copy_github_pull_request_template
+  copy_file 'github/pull_request_template.md', '.github/pull_request_template.md', force: true
+end
+
+apply_template!
