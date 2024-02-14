8152b9136a2459b946efe2824289f8a1988c9e3d
Alexey Solilin <solilin@jetrockets.com>
2024-02-14 19:26:14 +0100

    Add TTL for production

--- application/config/environments/production.rb
+++ application/config/environments/production.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
 uncomment_lines 'config/environments/production.rb', /config\.force_ssl = true/
+
+public_file_server_regex = /config\.public_file_server\.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?\n/
+
+insert_into_file 'config/environments/production.rb', after: public_file_server_regex do
+  <<-RUBY
+
+  # Ensure mailer works in development.
+  config.public_file_server.headers = ENV["RAILS_SERVE_STATIC_FILES"].present? && {
+    "Cache-Control" => "public, max-age=15552000"
+  }
+  RUBY
+end
